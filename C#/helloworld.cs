1a52ca996fe57a68b3362dfb4c53476f0aa60735
supergamza123 <141005387+supergamza123@users.noreply.github.com>
2024-12-24 14:47:24 +0900

    c# hello world

---
+++ C#/helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
